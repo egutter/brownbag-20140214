f5c54def583688d275c960391a03980016bb277d
Kevin Musiorski <kmusiorski@connectedhealth.com>
2014-03-28 14:26:14 -0500

    Add a tennis board for 0-0 score

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -1,7 +1,16 @@
 require File.dirname(__FILE__) + "/../app/foo"
 
-describe Foo do
-  it "says hello" do
-    subject.say_hello('Emilio').should == 'Hello Emilio'
+class TennisBoard
+
+  def score
+    [0,0]
+  end
+end
+
+describe 'tennis scorer' do
+
+  it "should answer a score of 0-0 if the match does not have any points" do
+    board = TennisBoard.new
+    board.score.should == [0, 0]
   end
 end
